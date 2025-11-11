680f3abd3acdd0526286d033d7b3893614e388be
Konstantin Kurshin <ekurshin@gmail.com>
2025-11-11 12:36:04 +0300

    fix conftest.py

--- conftest.py
+++ conftest.py
@@ -1,25 +1,3 @@
-# import pytest
-# from data.endpoints import Endpoints, send_request
-# from data.user_data import User
-
-
-# @pytest.fixture(scope="function")
-# def create_user():
-#     """Создаёт нового пользователя и возвращает токен"""
-#     payload = User.create_data_user()
-#     login_data = payload.copy()
-#     del login_data["name"]
-
-#     response = send_request("POST", Endpoints.CREATE_USER, json=payload)
-#     assert response.status_code == 200, f"Ошибка при создании пользователя: {response.text}"
-
-#     token = response.json()["accessToken"]
-#     yield response, payload, login_data, token
-
-#     send_request("DELETE", Endpoints.DELETE_USER, headers={"Authorization": token})
-    
-
-
 import pytest
 from data.endpoints import Endpoints, send_request
 from data.user_data import User
